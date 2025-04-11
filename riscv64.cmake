4cb5940c22553526207e340cb0195af5d181a2c5
Piotr Borczyk <howorang@gmail.com>
2025-04-12 01:54:19 +0200

    Transitioned from Makefile to CMake

---
+++ riscv64.cmake
@@ -0,0 +1,12 @@
+# set up cross compiling
+set(CMAKE_SYSTEM_NAME	  Generic)
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
+set(TOOLCHAIN_PREFIX    riscv64-unknown-elf-)
+set(CMAKE_C_COMPILER    "${TOOLCHAIN_PREFIX}gcc")
+set(CMAKE_CXX_COMPILER	"${TOOLCHAIN_PREFIX}g++")
+set(CMAKE_ASM_COMPILER	"${TOOLCHAIN_PREFIX}as")
+set(CMAKE_LINKER        "${TOOLCHAIN_PREFIX}ld")
+set(CMAKE_OBJCOPY       "${TOOLCHAIN_PREFIX}objcopy")
+
+set(CROSSCOMPILING_EMULATOR qemu-system-riscv64)
