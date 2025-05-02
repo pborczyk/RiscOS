9d6c2034b69b6065775bbd37b68c48bef724bb6e
Piotr Borczyk <howorang@gmail.com>
2025-05-02 20:37:50 +0200

    Added runscript

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+qemu-system-riscv64 -M virt \
+-display none -serial stdio \
+-bios bin/u-boot-spl.bin \
+-device loader,file=bin/u-boot.itb,addr=0x80200000 \
+-netdev user,id=n0,tftp=./${BUILD_DIR},bootfile=kernel.bin -device virtio-net-device,netdev=n0
