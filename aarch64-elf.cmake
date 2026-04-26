77f1422d04e74c8c07f4c087bd1591a1a27c005a
Piotr Borczyk <howorang@gmail.com>
2026-04-26 21:31:43 +0200

    CMake config files for vscode

--- aarch64-elf.cmake
+++ aarch64-elf.cmake
@@ -2,22 +2,20 @@
 set(CMAKE_SYSTEM_NAME	  Generic)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(TOOLCHAIN_PREFIX    aarch64-elf-)
-set(CMAKE_C_COMPILER    "${TOOLCHAIN_PREFIX}gcc")
-set(CMAKE_CXX_COMPILER	"${TOOLCHAIN_PREFIX}g++")
-set(CMAKE_ASM_COMPILER	"${TOOLCHAIN_PREFIX}as")
-set(CMAKE_LINKER        "${TOOLCHAIN_PREFIX}ld")
-set(CMAKE_OBJCOPY       "${TOOLCHAIN_PREFIX}objcopy")
+set(TOOLCHAIN_PREFIX    aarch64-none-elf-)
+set(TOOLCHAIN_DIRECTORY /home/howorang/tools/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf/bin/)
+
+set(CMAKE_C_COMPILER    "${TOOLCHAIN_DIRECTORY}${TOOLCHAIN_PREFIX}gcc")
+set(CMAKE_CXX_COMPILER	"${TOOLCHAIN_DIRECTORY}${TOOLCHAIN_PREFIX}g++")
+set(CMAKE_ASM_COMPILER	"${TOOLCHAIN_DIRECTORY}${TOOLCHAIN_PREFIX}as")
+set(CMAKE_LINKER        "${TOOLCHAIN_DIRECTORY}${TOOLCHAIN_PREFIX}ld")
+set(CMAKE_OBJCOPY       "${TOOLCHAIN_DIRECTORY}${TOOLCHAIN_PREFIX}objcopy")
 
 set(CROSSCOMPILING_EMULATOR qemu-system-aarch64)
-
-
-# where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /Users/howorang/Projects/RiscOSEnv/)
 
 # search headers and libraries in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_INCLUDE_PATH /opt/homebrew/Cellar/aarch64-elf-gcc/15.2.0/lib/gcc/aarch64-elf/15.2.0/include)
+set(CMAKE_INCLUDE_PATH /home/howorang/tools/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf/include)
