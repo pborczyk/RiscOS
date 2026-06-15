4055ddd3e1426166b5b6cb5fc4207269a38750b6
Piotr Borczyk <howorang@gmail.com>
2026-06-16 00:41:23 +0200

    Simplified debbuging

--- kernel.c
+++ kernel.c
@@ -2,7 +2,6 @@
 #include "memory.h"
 
 void kmain(void) {
-     init_allocator();
      printf("Memory allocator initialized");
      printf("Hello world! %d\n", 11);
 }
--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-qemu-system-aarch64 -machine virt -cpu cortex-a57 -kernel build/Kernel.elf -nographic -s -S
+#!/bin/bash
+qemu-system-aarch64 -machine virt -cpu cortex-a57 -kernel build/Kernel.elf -nographic -s -S &
+PID=$!
+until nc -z localhost 1234 2>/dev/null; do sleep 0.1; done
+echo 'QEMU ready (GDB on :1234)'
+wait $PID
